2ab27b70d1067e7bf0c0f1543581f8f8dd3280f2
Honseo <baum123@naver.com>
2014-10-10 21:50:44 +0900

    real email send

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -6,6 +6,18 @@ class EmailController < ApplicationController
   def email_send
     @email=params[:email]
     @content=params[:content]
+    @title=params[:title]
+    
+    gmail = Gmail.connect("honseoson@gmail.com","ghtjd159")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+    
+    gmail.logout
   end
   
 end
