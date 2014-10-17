d7499fa258bb3dbda62de40a88150288a9e14fad
Honseo <baum123@naver.com>
2014-10-18 02:07:04 +0900

    number display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,21 @@ class HomeController < ApplicationController
   def hello
     @name=params[:my_name]
   end
+  
+  def list
+    @number = Number.all
+  end
+  
+  def add
+  end
+  
+  def add_process
+    
+    n = Number.new
+    n.name = params[:phone_user]
+    n.phone_number = params [:phone_number]
+    n.save
+    
+  end
+  
 end
